53af1712182fe2e4d754af71802c4699f2542ae3
Mathieu Robichaud <admin@birkoss.com>
2020-12-18 14:11:54 -0500

    Basic templates + Social Auth

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include, re_path
+
+from gamebook import views as gamebook_views
 
 urlpatterns = [
+    path('', gamebook_views.home, name='home'),
+    path('login/', gamebook_views.login, name='login'),
     path('admin/', admin.site.urls),
-]
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/managers.py
+++ users/managers.py
@@ -2,13 +2,14 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email must be set')
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+        if password is not None:
+            user.set_password(password)
         user.save()
 
         return user
